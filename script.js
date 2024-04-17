f78b9d3a6d6707c6401165b31247d3f977027188
deeprathod97 <deeprathod9797@gmail.com>
2024-04-17 12:41:19 +0530

    nedted if statement

---
+++ script.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = 5;
+
+if (x > 5) {
+  console.log("x is greater than 5");
+
+  if (y > 2) {
+    console.log("y is greater than 2");
+  } else {
+    console.log("y is not greater than 2");
+  }
+} else {
+  console.log("x is not greater than 5");
+}
